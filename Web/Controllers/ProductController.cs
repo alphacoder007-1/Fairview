737d8d4c0505a3f0cb2c421d1ed5915cd6ff1335
mshivaswamy <mahesh.shivaswamy@zumepizza.com>
2020-04-05 20:04:33 -0700

    add crud operation

--- Web/Controllers/ProductController.cs
+++ Web/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using DAL;
 using Microsoft.AspNetCore.Mvc;
+using Web.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -19,8 +20,66 @@ namespace Web.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            var products = db.Products.ToList();
+            // var products = db.Products.ToList();
+
+            var products = (from p in db.Products
+                            join c in db.Categories
+                            on p.CategoryId equals c.CategoryId
+                            select new ProductViewModel
+                            {
+                                ProductId = p.ProductId,
+                                Name = p.Name,
+                                UnitPrice = p.Unitprice,
+                                Description = p.Description,
+                                Category = c.Name
+                            }).ToList();
+
+
             return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            ViewBag.Categories = db.Categories.ToList();
+            return View();       
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product model)
+        {
+            try
+            {
+                db.Products.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+
+                ViewBag.Message = $"Something went wrong : {ex.Message}";
+            }
+            ViewBag.Category = db.Categories.ToList();
+            return View();
+        }
+
+
+        public IActionResult Edit(int Id)
+        {
+             ViewBag.Categories = db.Categories.ToList();
+            Product prd = db.Products.Where(p => p.ProductId == Id).FirstOrDefault();
+            return View("Create",prd);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var product = db.Products.Where(p => p.ProductId == id).FirstOrDefault();
+            if (product != null)
+            {
+                db.Products.Remove(product);
+                db.SaveChanges();
+            }
+            
+            return RedirectToAction("Index");
         }
     }
 }
